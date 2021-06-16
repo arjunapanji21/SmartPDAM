ec1f40482cb185ca2c413fa77248b35f4fa6fc40
arjunapanji21 <arjuna.panji97@gmail.com>
2021-06-16 16:56:49 +0700

    nodemcu selesai

--- waterflow-nodemcu/waterflow-nodemcu.ino
+++ waterflow-nodemcu/waterflow-nodemcu.ino
@@ -10,11 +10,11 @@
 
 #define FIREBASE_HOST   "smartpdam-961a8-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH   "BbwxzHboCN7ULeHJUuRzpK8jhoBGtsDHdxxSOSI8"
-#define WIFI_SSID       "Dea Lova"
-#define WIFI_PASSWORD   "de@lova197050"
+#define WIFI_SSID       "UNAMA"
+#define WIFI_PASSWORD   "unamaoke"
+#define WF_SENSOR       D3
 
-//#define USER_ID         "users/PO2NWeXFMPQaxDlU5c609kDSVJf2/"
-String USER_ID = "users/PO2NWeXFMPQaxDlU5c609kDSVJf2/";
+String USER_ID = "users/xI3UWpcMSJXeqP8fqfIlG0w3fPn1/"; // ubah userID
 
 // define NTP Client to get time
 WiFiUDP ntpUDP;
@@ -29,6 +29,14 @@ String months[12] = {
   "January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 };
+
+long curr_ms = 0, prev_ms = 0, interval = 1000, ml, total_ml;
+float calibrationFactor = 4.5, flowRate = 0, flowLiter = 0, totalLiter = 0, air, biaya;
+byte pulseCount = 0, pulsePerSec = 0;
+
+void IRAM_ATTR pulseCounter() {
+  pulseCount++;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -54,38 +62,64 @@ void setup() {
   timeClient.setTimeOffset(3600 * 7); // GMT +7
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  pinMode(WF_SENSOR, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(WF_SENSOR), pulseCounter, FALLING);
+
+  air = getAir().toFloat();
+  biaya = getBiaya().toFloat();
 }
 
 void loop() {
-  float air = getAir().toFloat();
-  float biaya = getBiaya().toFloat();
-  air = air;
+  curr_ms = millis();
+  if (curr_ms - prev_ms > interval) {
+
+    pulsePerSec = pulseCount;
+    pulseCount = 0;
+    flowRate = ((1000.0 / (millis() - prev_ms)) * pulsePerSec) / calibrationFactor;
+    prev_ms = millis();
+
+    ml = (flowRate / 60) * 1000;
+    flowLiter = (flowRate / 60);
+    total_ml += ml;
+
+    totalLiter += flowLiter;
+  }
+
+  if (flowRate > 0) {
+    air += totalLiter;
+  }
+
+  Serial.print("Update Data Air: ");
+  Serial.print(air);
+  Serial.print("\t");
+  Serial.print(flowRate);
+  Serial.print("\t");
+  Serial.println(flowLiter);
+
   if (air <= 10000) {
-    biaya = air * 2.6;
+    biaya = air * 4;
   }
-  else if (air <= 20000) {
-    biaya = air * 4.6;
-  }
-  else if (air <= 30000) {
-    biaya = air * 7.4;
+  else if (air > 10000 && air <= 20000) {
+    biaya = air * 5.5;
   }
   else {
-    biaya = air * 10.7;
+    biaya = air * 6.5;
   }
+
+  Serial.print("Update Data Biaya: Rp.");
+  Serial.println(biaya);
+  Serial.println("");
+
   updateAir(air);
   updateBiaya(biaya);
-//  Serial.print("Update Data Air: ");
-//  Serial.println(air);
-//  Serial.print("Update Data Biaya: Rp.");
-//  Serial.println(biaya);
-//  Serial.println("");
-  delay(5);
 }
 
 void updateAir(int value) {
   String air = String(value);
   String bulan = String(getBulan());
-  
+
   // update data air
   String pathAir = USER_ID + "data/" + bulan + "/air";
   Firebase.setString(pathAir, air); checkError();
@@ -93,15 +127,17 @@ void updateAir(int value) {
   // update chart
   String pathChart = USER_ID + "chart/" + bulan + "/y";
   Firebase.setString(pathChart, air); checkError();
+  Serial.println("Berhasil Update Air");
 }
 
 void updateBiaya(int value) {
   String biaya = String(value);
   String bulan = String(getBulan());
-  
+
   // update biaya
   String pathBiaya = USER_ID + "data/" + bulan + "/biaya";
   Firebase.setString(pathBiaya, biaya); checkError();
+  Serial.println("Berhasil Update Biaya");
 }
 
 String getAir() {
